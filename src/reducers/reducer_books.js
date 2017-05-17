58147799c198cf4e01f330364be0900b739e6236
yonatana <yonatana@jfrog.com>
2017-05-17 16:31:11 +0300

    com

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+	return [
+	{title: 'Javscript: The good parts'},
+	{title: 'Harry Potter'},
+	{title: 'Game of Thrones'},
+	{title: 'Bible'},
+	{title: 'Quran'}
+	];
+}
+
